786450825c317cc922f75355412192e6a94e91f6
Aleksandra Danilov <aleksandrasdanilov@gmail.com>
2020-12-12 08:43:16 +0100

    Initial Commit

---
+++ UsersProfileCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsersProfileCard
+//
+//  Created by user185274 on 12/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UsersProfileCard/UsersProfileCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersProfileCardApp.swift
+//  UsersProfileCard
+//
+//  Created by user185274 on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct UsersProfileCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
